a42fb72ed1544c886156393ea5568f1b17ebc4ca
Ovidiu <ovidiu.sbangu@gmail.com>
2023-10-05 11:14:41 +0300

    Complete stage2. generate matrix based on input

--- simple_tic_tac_toe/main.py
+++ simple_tic_tac_toe/main.py
@@ -1,10 +1,15 @@
 """Main module"""
-from simple_tic_tac_toe import print_matrix  # type: ignore
+from simple_tic_tac_toe import generate_matrix, print_matrix  # type: ignore
 
 
 def main() -> None:
     """Main function"""
-    print_matrix()
+
+    text = input()
+
+    matrix = generate_matrix(text)
+    # print(matrix)
+    print_matrix(matrix)
 
 
 if __name__ == "__main__":
--- simple_tic_tac_toe/simple_tic_tac_toe.py
+++ simple_tic_tac_toe/simple_tic_tac_toe.py
@@ -1,9 +1,32 @@
 """Hello module"""
 
+def generate_matrix(text: str) -> list[list[str]]:
+    """
+    Generate 3x3 Matrix
+    :param text: text containing X, 0 or _
+    :return: matrix list
+    """
+    matrix = []
+    max_col = 3
+    for i in range(0, len(text), max_col):
+        row = [ text[j] for j in range(i, max_col + i) ]
+        matrix.append(row)
 
-def print_matrix() -> None:
-    print("""
-    X O X
-    O X O
-    X X O 
-    """)
+    return matrix
+
+def print_matrix(matrix: list[list[str]]) -> None:
+    """
+    Print Matrix
+    :param matrix:
+    :return: None
+    """
+    print("---------")
+    for i, row in enumerate(matrix):
+        print("|", end=" ")
+        for j, col in enumerate(row):
+            item = row[j]
+            print(item, end=" ")
+        print("|", end=" ")
+        print()
+    print("---------")
+
