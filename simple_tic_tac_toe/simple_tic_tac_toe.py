c363b23f90623b3fe7d5b4b20ee9e879bfcab3d3
Ovidiu <ovidiu.sbangu@gmail.com>
2023-10-18 14:04:09 +0300

    Refactor some code / complete stage3 / added unit tests + 100% cov

--- simple_tic_tac_toe/simple_tic_tac_toe.py
+++ simple_tic_tac_toe/simple_tic_tac_toe.py
@@ -1,32 +1,93 @@
-"""Hello module"""
+MAX_GRID = 3
 
-def generate_matrix(text: str) -> list[list[str]]:
-    """
-    Generate 3x3 Matrix
-    :param text: text containing X, 0 or _
-    :return: matrix list
-    """
-    matrix = []
-    max_col = 3
-    for i in range(0, len(text), max_col):
-        row = [ text[j] for j in range(i, max_col + i) ]
-        matrix.append(row)
 
-    return matrix
+def create_grid(user_input: str) -> list[list[str]]:
+    """
+    Create 3x3 Game Grid
+    @param user_input: string containing X's / O's and/or empty lines
+    @return: 3X3 Game Grid
+    """
+    grid = []
 
-def print_matrix(matrix: list[list[str]]) -> None:
+    for i in range(0, len(user_input), MAX_GRID):
+        row = [user_input[j] for j in range(i, MAX_GRID + i)]
+        grid.append(row)
+
+    return grid
+
+
+def is_row_winner(grid: list[list[str]], player: str) -> bool:
+    for row in grid:
+        if row.count(player) == MAX_GRID:
+            return True
+
+    return False
+
+
+def is_col_winner(grid: list[list[str]], player: str) -> bool:
+    for i in range(len(grid)):
+        count = 0
+        for j in range(len(grid[i])):
+            if grid[j][i] == player:
+                count += 1
+        if count == MAX_GRID:
+            return True
+
+    return False
+
+
+def is_diag_winner(grid: list[list[str]], player: str) -> bool:
+    count = 0
+    # check diag
+    for i in range(MAX_GRID):
+        if grid[i][i] == player:
+            count += 1
+
+    if count == 3:
+        return True
+
+    # check reverse diag
+    count = 0
+    for i in range(MAX_GRID):
+        if grid[i][MAX_GRID - 1 - i] == player:
+            count += 1
+
+    return count == 3
+
+
+def is_cell_empty(grid: list[list[str]]) -> bool:
+    for row in grid:
+        for col in row:
+            if col == "_":
+                return True
+
+    return False
+
+
+def get_player_diff(grid: list[list[str]]) -> int:
+    player_X = 0
+    player_O = 0
+
+    for row in grid:
+        for col in row:
+            if col == "X":
+                player_X += 1
+            elif col == "O":
+                player_O += 1
+
+    return player_X - player_O if player_X > player_O else player_O - player_X
+
+
+def print_grid(grid: list[list[str]]) -> None:
     """
-    Print Matrix
-    :param matrix:
-    :return: None
+    Print Game state / grid
+    @param grid: 3x3 grid
     """
-    print("---------")
-    for i, row in enumerate(matrix):
+    print("-" * len(grid) * MAX_GRID)
+    for row in grid:
         print("|", end=" ")
-        for j, col in enumerate(row):
-            item = row[j]
-            print(item, end=" ")
+        for col in row:
+            print(f"{col}", end=" ")
         print("|", end=" ")
         print()
-    print("---------")
-
+    print("-" * len(grid) * MAX_GRID)
--- tests/test_simple_tic_tac_toe.py
+++ tests/test_simple_tic_tac_toe.py
@@ -1,4 +1,96 @@
-from simple_tic_tac_toe.simple_tic_tac_toe import hello
+import pytest
+
+from simple_tic_tac_toe.simple_tic_tac_toe import (
+    create_grid,
+    get_player_diff,
+    is_cell_empty,
+    is_col_winner,
+    is_diag_winner,
+    is_row_winner,
+)
 
 
+@pytest.mark.parametrize(
+    "test_inp_, expected",
+    [
+        (("XXXOO__O_", "X"), True),
+        (("___XXX___", "X"), True),
+        (("___OO_XXX", "X"), True),
+        (("OOOXX__O_", "O"), True),
+        (("___OOO___", "O"), True),
+        (("___XX_OOO", "O"), True),
+        (("XXXOO__O_", "O"), False),
+        (("___XXX___", "O"), False),
+        (("___OO_XXX", "O"), False),
+        (("OOOXX__O_", "X"), False),
+        (("___OOO___", "X"), False),
+        (("___XX_OOO", "X"), False),
+    ],
+)
+def test_row_winner(test_inp_, expected):
+    text, player = test_inp_
+    grid = create_grid(text)
+    assert is_row_winner(grid, player) == expected
 
+
+@pytest.mark.parametrize(
+    "test_inp_, expected",
+    [
+        (("X__XOXXO_", "X"), True),
+        (("_X_OXO_X_", "X"), True),
+        (("__X_OXO_X", "X"), True),
+        (("O__OXOOX_", "O"), True),
+        (("_O_XOX_O_", "O"), True),
+        (("__O_XO_XO", "O"), True),
+        (("X__XOXXO_", "O"), False),
+        (("_X_OXO_X_", "O"), False),
+        (("__X_OXO_X", "O"), False),
+        (("O__OXOOX_", "X"), False),
+        (("_O_XOX_O_", "X"), False),
+        (("__O_XO_XO", "X"), False),
+    ],
+)
+def test_col_winner(test_inp_, expected):
+    text, player = test_inp_
+    grid = create_grid(text)
+    assert is_col_winner(grid, player) == expected
+
+
+@pytest.mark.parametrize(
+    "test_inp_, expected",
+    [
+        (("X__OX_OOX", "X"), True),
+        (("O__XO_X_O", "O"), True),
+        (("X__OX_OO_", "X"), False),
+        (("__XOX_XOX", "X"), True),
+        (("__O_OOOX_", "O"), True),
+        (("__XOX_OO_", "X"), False),
+    ],
+)
+def test_diag_winner(test_inp_, expected):
+    text, player = test_inp_
+    grid = create_grid(text)
+    assert is_diag_winner(grid, player) == expected
+
+
+@pytest.mark.parametrize(
+    "test_inp_, expected",
+    [
+        ("X__OXOOOX", True),
+        ("OXXXO_XOO", True),
+        ("XXXOXOXO_", True),
+        ("XXXOXOXOX", False),
+    ],
+)
+def test_cell_empty(test_inp_, expected):
+    grid = create_grid(test_inp_)
+    assert is_cell_empty(grid) == expected
+
+
+@pytest.mark.parametrize(
+    "test_inp_, expected",
+    [("_O_X__X_X", 2), ("_OOOO_X_X", 2)],
+)
+def test_get_player_diff(test_inp_, expected):
+    grid = create_grid(test_inp_)
+    assert get_player_diff(grid) == expected
